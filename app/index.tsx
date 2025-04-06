9613716cb431b7db930d9d872bc9527e61d1f2ae
Mouzone <sunnyliu010@gmail.com>
2025-04-05 22:18:13 -0400

    Create home screen with some options

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,41 @@
 import { useRouter } from "expo-router";
-import { Button, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
     const router = useRouter();
+    // todo: group the categories into Random Distribution, Divisible by, Patterns
     return (
-        <View>
-            <Button
-                onPress={() => router.navigate("/game", { id: "even" })}
-                title="Even"
-            />
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 20,
+            }}
+        >
+            <Pressable onPress={() => router.navigate("/game", { id: "even" })}>
+                <Text> Even </Text>
+            </Pressable>
+            <Pressable onPress={() => router.navigate("/game", { id: "odd" })}>
+                <Text> Odd </Text>
+            </Pressable>
+            <Pressable
+                onPress={() => router.navigate("/game", { id: "fibonacci" })}
+            >
+                <Text> Fibonacci </Text>
+            </Pressable>
+            <Pressable
+                onPress={() => router.navigate("/game", { id: "prime" })}
+            >
+                <Text> Prime </Text>
+            </Pressable>
+            <Pressable
+                onPress={() =>
+                    router.navigate("/game", { id: "normal distribution" })
+                }
+            >
+                <Text> Normal Distribution </Text>
+            </Pressable>
         </View>
     );
 }
